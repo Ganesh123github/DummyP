99b53b18cd70d5a70252d09c6823c4417f1b223a
Ganesh Raut <rautg95@gmail.com>
2022-09-13 15:51:34 +0530

    first commit

---
+++ Dummyproject/src/Demo1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Demo1 {
+ public static void main(String []args)
+ {
+	ArrayList al = new ArrayList(); 
+	 al.add(12);
+	 al.add(22);
+	 al.add("ganesh");
+	 al.add("raut");
+	 
+	 Iterator itr = al.iterator();
+	 while(itr.hasNext())
+	 {
+		System.out.println(itr.next()) ;
+		 
+	 }
+	 
+	 
+	 
+ }
+}
